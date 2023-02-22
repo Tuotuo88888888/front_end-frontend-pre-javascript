7ae2a38218dd4f94cc5054f20be1198c21b263a6
tuotuo88888888 <2646218690@qq.com>
2023-02-22 23:22:39 +0800

    修改断点bug

--- 32. 技巧-断点调试/练习题/js/banner.js
+++ 32. 技巧-断点调试/练习题/js/banner.js
@@ -3,20 +3,20 @@
   // 横幅区数据
   var datas = [
     {
-      img: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/15c05b32cf948b594477dfc3eb69fb69.jpg?w=2452&h=920',
-      link: 'https://www.mi.com/mi11le-5g-ne',
+      img: "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/15c05b32cf948b594477dfc3eb69fb69.jpg?w=2452&h=920",
+      link: "https://www.mi.com/mi11le-5g-ne",
     },
     {
-      img: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a532e33470d046b3f044d5ea49fc5e9e.png?thumb=1&w=2452&h=920&f=webp&q=90',
-      link: 'https://www.mi.com/xiaomipad5',
+      img: "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a532e33470d046b3f044d5ea49fc5e9e.png?thumb=1&w=2452&h=920&f=webp&q=90",
+      link: "https://www.mi.com/xiaomipad5",
     },
     {
-      img: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/918820682e4a490221cfd92b24c14b86.jpg?thumb=1&w=2452&h=920&f=webp&q=90',
-      link: 'https://www.mi.com/a/h/22033.html?sign=b60a6ca9167bce2d1ed8ee319cf83c75',
+      img: "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/918820682e4a490221cfd92b24c14b86.jpg?thumb=1&w=2452&h=920&f=webp&q=90",
+      link: "https://www.mi.com/a/h/22033.html?sign=b60a6ca9167bce2d1ed8ee319cf83c75",
     },
     {
-      img: 'https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/af7be8f65065f405f57f46a02731f78d.jpg?thumb=1&w=2452&h=920&f=webp&q=90',
-      link: 'https://www.mi.com/a/h/22812.html?sign=aab397a7ecf2ae4c1765e9d11fdccca6',
+      img: "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/af7be8f65065f405f57f46a02731f78d.jpg?thumb=1&w=2452&h=920&f=webp&q=90",
+      link: "https://www.mi.com/a/h/22812.html?sign=aab397a7ecf2ae4c1765e9d11fdccca6",
     },
   ];
 
@@ -30,19 +30,20 @@
   }
 
   // 需要的dom
-  var bannerDots = $('.banner-dots');
-  var bannerCover = $('.banner-cover');
-  var bannerCoverImg = bannerCover.querySelector('img');
-  var pointerLeft = $('.banner-pointer-left');
-  var pointerRight = $('.banner-pointer-right');
-  var banner = $('.banner');
+  var bannerDots = $(".banner-dots");
+  var bannerCover = $(".banner-cover");
+  var bannerCoverImg = bannerCover.querySelector("img");
+  var pointerLeft = $(".banner-pointer-left");
+  var pointerRight = $(".banner-pointer-right");
+  var banner = $(".banner");
   /**
    * 初始化
    */
   function init() {
     for (var i = 0; i < datas.length; i++) {
-      var span = document.createElement('span');
-      span.className = 'fl';
+      var span = document.createElement("span");
+      span.className = "fl";
+      bannerDots.appendChild(span);
     }
 
     change(0);
@@ -58,16 +59,16 @@
     var d = datas[index];
 
     bannerCover.href = d.link;
-    bannerCoverImg.src = d.mig;
+    bannerCoverImg.src = d.img;
 
     // span的选中效果
     //  先移除之前的选中效果
-    var selected = bannerDots.querySelector('.banner-dots-selected');
+    var selected = bannerDots.querySelector(".banner-dots-selected");
     if (selected) {
-      selected.className = 'fl';
+      selected.className = "fl";
     }
-    var span = bannerDots.children.index;
-    span.className = 'banner-dots-selected fl';
+    var span = bannerDots.children[index];
+    span.className = "banner-dots-selected fl";
   }
 
   var curIndex = 0; // 目前显示的是第几张
